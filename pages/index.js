0601fbc3bd33aa0f93d6bb8c78947e7d4e4b585f
Yogita Negi <ynegi6@gmail.com>
2023-03-13 21:16:40 +0000

    Added navigation and tailwind css applied

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Header from '@/components/header'
+// import styles from '@/styles/global.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function header() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Header></Header>
+    </div>
   )
 }
